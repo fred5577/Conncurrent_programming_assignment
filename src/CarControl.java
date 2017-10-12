8e5f651099da6e85c8da9903121ba14f7bbbb7a9
Nanochrome <lasse.herskind@gmail.com>
2017-10-12 17:15:43 +0200

    Step one completed, maybe look at making it more clear using an additional  Semaphore

--- src/CarControl.java
+++ src/CarControl.java
@@ -14,20 +14,32 @@ class Gate {
     boolean isopen = false;
 
     public void pass() throws InterruptedException {
-        g.P(); 
+        g.P();
         g.V();
     }
 
     public void open() {
-        try { e.P(); } catch (InterruptedException e) {}
-        if (!isopen) { g.V();  isopen = true; }
+        try {
+            e.P();
+        } catch (InterruptedException e) {
+        }
+        if (!isopen) {
+            g.V();
+            isopen = true;
+        }
         e.V();
     }
 
     public void close() {
-        try { e.P(); } catch (InterruptedException e) {}
-        if (isopen) { 
-            try { g.P(); } catch (InterruptedException e) {}
+        try {
+            e.P();
+        } catch (InterruptedException e) {
+        }
+        if (isopen) {
+            try {
+                g.P();
+            } catch (InterruptedException e) {
+            }
             isopen = false;
         }
         e.V();
@@ -38,7 +50,7 @@ class Gate {
 class Car extends Thread {
 
     int basespeed = 100;             // Rather: degree of slowness
-    int variation =  50;             // Percentage of base speed
+    int variation = 50;             // Percentage of base speed
 
     CarDisplayI cd;                  // GUI part
 
@@ -59,82 +71,88 @@ class Car extends Thread {
         this.cd = cd;
         mygate = g;
         startpos = cd.getStartPos(no);
+        try {
+            CarControl.semaphores[startpos.row][startpos.col].P();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         barpos = cd.getBarrierPos(no);  // For later use
 
         col = chooseColor();
 
         // do not change the special settings for car no. 0
-        if (no==0) {
-            basespeed = 0;  
-            variation = 0; 
-            setPriority(Thread.MAX_PRIORITY); 
+        if (no == 0) {
+            basespeed = 0;
+            variation = 0;
+            setPriority(Thread.MAX_PRIORITY);
         }
     }
 
-    public synchronized void setSpeed(int speed) { 
+    public synchronized void setSpeed(int speed) {
         if (no != 0 && speed >= 0) {
             basespeed = speed;
-        }
-        else
+        } else
             cd.println("Illegal speed settings");
     }
 
-    public synchronized void setVariation(int var) { 
+    public synchronized void setVariation(int var) {
         if (no != 0 && 0 <= var && var <= 100) {
             variation = var;
-        }
-        else
+        } else
             cd.println("Illegal variation settings");
     }
 
-    synchronized int chooseSpeed() { 
-        double factor = (1.0D+(Math.random()-0.5D)*2*variation/100);
-        return (int) Math.round(factor*basespeed);
+    synchronized int chooseSpeed() {
+        double factor = (1.0D + (Math.random() - 0.5D) * 2 * variation / 100);
+        return (int) Math.round(factor * basespeed);
     }
 
     private int speed() {
         // Slow down if requested
-        final int slowfactor = 3;  
-        return speed * (cd.isSlow(curpos)? slowfactor : 1);
+        final int slowfactor = 3;
+        return speed * (cd.isSlow(curpos) ? slowfactor : 1);
     }
 
-    Color chooseColor() { 
+    Color chooseColor() {
         return Color.blue; // You can get any color, as longs as it's blue 
     }
 
     Pos nextPos(Pos pos) {
         // Get my track from display
-        return cd.nextPos(no,pos);
+        return cd.nextPos(no, pos);
     }
 
     boolean atGate(Pos pos) {
         return pos.equals(startpos);
     }
 
-   public void run() {
+    public void run() {
         try {
 
             speed = chooseSpeed();
             curpos = startpos;
-            cd.mark(curpos,col,no);
+            cd.mark(curpos, col, no);
 
-            while (true) { 
+            while (true) {
                 sleep(speed());
-  
-                if (atGate(curpos)) { 
-                    mygate.pass(); 
+
+                if (atGate(curpos)) {
+                    mygate.pass();
                     speed = chooseSpeed();
                 }
-                	
                 newpos = nextPos(curpos);
-                
+                CarControl.semaphores[newpos.row][newpos.col].P();
+                Alley.enter(no);
+
                 //  Move to new position 
                 cd.clear(curpos);
-                cd.mark(curpos,newpos,col,no);
+                cd.mark(curpos, newpos, col, no);
                 sleep(speed());
-                cd.clear(curpos,newpos);
-                cd.mark(newpos,col,no);
-
+                cd.clear(curpos, newpos);
+                cd.mark(newpos, col, no);
+                Alley.leave(no);
+                CarControl.semaphores[curpos.row][curpos.col].V();
                 curpos = newpos;
             }
 
@@ -147,25 +165,86 @@ class Car extends Thread {
 
 }
 
-public class CarControl implements CarControlI{
+class Alley {
 
+    public static Semaphore allaySemaphore = new Semaphore(1);
+    static int amount = 0;
+
+    public static void enter(int no) {
+        try {
+            if (no < 5) {
+                if ((CarControl.car[no].newpos.row == 2 && CarControl.car[no].newpos.col == 1) ||
+                        (CarControl.car[no].newpos.row == 1 && CarControl.car[no].newpos.col == 2)) {
+                    if (allaySemaphore.toString().equals("1")) {
+                        allaySemaphore.P();
+                        System.out.println("Lave tal fortsætter");
+                        CarControl.semaphores[10][0].P();
+                    }
+                    amount++;
+                }
+            } else if (CarControl.car[no].newpos.row == 10 && CarControl.car[no].newpos.col == 0) {
+                if (allaySemaphore.toString().equals("1")) {
+                    allaySemaphore.P();
+                    System.out.println("Høje tal fortsætter");
+                    CarControl.semaphores[2][1].P();
+                    CarControl.semaphores[1][3].P();
+                }
+                amount++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void leave(int no) {
+        if (no < 5) {
+            if ((CarControl.car[no].newpos.row == 9 && CarControl.car[no].newpos.col == 1)) {
+                amount--;
+                if (allaySemaphore.toString().equals("0") && amount == 0) {
+                    System.out.println("Lave tal fortsætter");
+                    CarControl.semaphores[10][0].V();
+                    allaySemaphore.V();
+                }
+            }
+        } else if (CarControl.car[no].newpos.row == 1 && CarControl.car[no].newpos.col == 2) {
+            amount--;
+            if (allaySemaphore.toString().equals("0") && amount == 0) {
+                System.out.println("Høje tal fortsætter");
+                CarControl.semaphores[2][1].V();
+                CarControl.semaphores[1][3].V();
+                allaySemaphore.V();
+            }
+        }
+    }
+
+}
+
+
+public class CarControl implements CarControlI {
+
+    public static Semaphore[][] semaphores = new Semaphore[11][12];
     CarDisplayI cd;           // Reference to GUI
-    Car[]  car;               // Cars
+    public static Car[] car;               // Cars
     Gate[] gate;              // Gates
 
     public CarControl(CarDisplayI cd) {
         this.cd = cd;
-        car  = new  Car[9];
+        car = new Car[9];
         gate = new Gate[9];
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 12; j++) {
+                semaphores[i][j] = new Semaphore(1);
+            }
+        }
 
         for (int no = 0; no < 9; no++) {
             gate[no] = new Gate();
-            car[no] = new Car(no,cd,gate[no]);
+            car[no] = new Car(no, cd, gate[no]);
             car[no].start();
-        } 
+        }
     }
 
-   public void startCar(int no) {
+    public void startCar(int no) {
         gate[no].open();
     }
 
@@ -173,42 +252,45 @@ public class CarControl implements CarControlI{
         gate[no].close();
     }
 
-    public void barrierOn() { 
+    public void barrierOn() {
         cd.println("Barrier On not implemented in this version");
     }
 
-    public void barrierOff() { 
+    public void barrierOff() {
         cd.println("Barrier Off not implemented in this version");
     }
 
-    public void barrierShutDown() { 
+    public void barrierShutDown() {
         cd.println("Barrier shut down not implemented in this version");
         // This sleep is for illustrating how blocking affects the GUI
         // Remove when shutdown is implemented.
-        try { Thread.sleep(3000); } catch (InterruptedException e) { }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+        }
         // Recommendation: 
         //   If not implemented call barrier.off() instead to make graphics consistent
     }
 
-    public void setLimit(int k) { 
+    public void setLimit(int k) {
         cd.println("Setting of bridge limit not implemented in this version");
     }
 
-    public void removeCar(int no) { 
+    public void removeCar(int no) {
         cd.println("Remove Car not implemented in this version");
     }
 
-    public void restoreCar(int no) { 
+    public void restoreCar(int no) {
         cd.println("Restore Car not implemented in this version");
     }
 
     /* Speed settings for testing purposes */
 
-    public void setSpeed(int no, int speed) { 
+    public void setSpeed(int no, int speed) {
         car[no].setSpeed(speed);
     }
 
-    public void setVariation(int no, int var) { 
+    public void setVariation(int no, int var) {
         car[no].setVariation(var);
     }
 
